b4663e97b5704e7f3d4a4d49cad02ed9fa0565f5
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-10-26 13:13:23 +0600

    Add files via upload

---
+++ BreadthFirstPaths.java
@@ -0,0 +1,201 @@
+package breadthfirstpaths;
+
+public class BreadthFirstPaths {
+    private static final int INFINITY = Integer.MAX_VALUE;
+    private boolean[] marked;  // marked[v] = is there an s-v path
+    private int[] edgeTo;      // edgeTo[v] = previous edge on shortest s-v path
+    private int[] distTo;      // distTo[v] = number of edges shortest s-v path
+
+
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        distTo = new int[G.V()];
+        edgeTo = new int[G.V()];
+        validateVertex(s);
+        bfs(G, s);
+
+        assert check(G, s);
+    }
+
+
+    public BreadthFirstPaths(Graph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        distTo = new int[G.V()];
+        edgeTo = new int[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = INFINITY;
+        validateVertices(sources);
+        bfs(G, sources);
+    }
+
+
+    // breadth-first search from a single source
+    private void bfs(Graph G, int s) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = INFINITY;
+        distTo[s] = 0;
+        marked[s] = true;
+        q.enqueue(s);
+
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                }
+            }
+        }
+    }
+
+    // breadth-first search from multiple sources
+    private void bfs(Graph G, Iterable<Integer> sources) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int s : sources) {
+            marked[s] = true;
+            distTo[s] = 0;
+            q.enqueue(s);
+        }
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return marked[v];
+    }
+
+    public int distTo(int v) {
+        validateVertex(v);
+        return distTo[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        validateVertex(v);
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<Integer>();
+        int x;
+        for (x = v; distTo[x] != 0; x = edgeTo[x])
+            path.push(x);
+        path.push(x);
+        return path;
+    }
+
+
+    // check optimality conditions for single source
+    private boolean check(Graph G, int s) {
+
+        // check that the distance of s = 0
+        if (distTo[s] != 0) {
+            System.out.println("distance of source " + s + " to itself = " + distTo[s]);
+            return false;
+        }
+
+        // check that for each edge v-w dist[w] <= dist[v] + 1
+        // provided v is reachable from s
+        for (int v = 0; v < G.V(); v++) {
+            for (int w : G.adj(v)) {
+                if (hasPathTo(v) != hasPathTo(w)) {
+                    System.out.println("edge " + v + "-" + w);
+                    System.out.println("hasPathTo(" + v + ") = " + hasPathTo(v));
+                    System.out.println("hasPathTo(" + w + ") = " + hasPathTo(w));
+                    return false;
+                }
+                if (hasPathTo(v) && (distTo[w] > distTo[v] + 1)) {
+                    System.out.println("edge " + v + "-" + w);
+                    System.out.println("distTo[" + v + "] = " + distTo[v]);
+                    System.out.println("distTo[" + w + "] = " + distTo[w]);
+                    return false;
+                }
+            }
+        }
+
+        // check that v = edgeTo[w] satisfies distTo[w] = distTo[v] + 1
+        // provided v is reachable from s
+        for (int w = 0; w < G.V(); w++) {
+            if (!hasPathTo(w) || w == s) continue;
+            int v = edgeTo[w];
+            if (distTo[w] != distTo[v] + 1) {
+                System.out.println("shortest path edge " + v + "-" + w);
+                System.out.println("distTo[" + v + "] = " + distTo[v]);
+                System.out.println("distTo[" + w + "] = " + distTo[w]);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    // throw an IllegalArgumentException if vertices is null, has zero vertices,
+    // or has a vertex not between 0 and V-1
+    private void validateVertices(Iterable<Integer> vertices) {
+        if (vertices == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+        int vertexCount = 0;
+        for (Integer v : vertices) {
+            vertexCount++;
+            if (v == null) {
+                throw new IllegalArgumentException("vertex is null");
+            }
+            validateVertex(v);
+        }
+        if (vertexCount == 0) {
+            throw new IllegalArgumentException("zero vertices");
+        }
+    }
+
+    /**
+     * Unit tests the {@code BreadthFirstPaths} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+         // TODO code application logic here
+        Graph g = new Graph(5);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(0, 2);
+        g.addEdge(2, 3);
+        
+        int s=0;
+        BreadthFirstPaths bfs = new BreadthFirstPaths(g, s);
+
+
+        for (int v = 0; v < g.V(); v++) {
+            if (bfs.hasPathTo(v)) {
+                System.out.print(s + " to " + v + " : " + "(" + bfs.distTo(v) + ")");
+                for (int x : bfs.pathTo(v)) {
+                    if (x == s) System.out.print(x);
+                    else        System.out.print("-" + x);
+                }
+                System.out.println();
+            }
+
+            else {
+                System.out.print(s + " to " + v + "(-):  not connected\n");
+            }
+        }
+    }
+
+
+}

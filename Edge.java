9d0ee22b9302da11a3b57d4c01e82cd692bdb50f
Md. Golam Kibria <78980142+kibria1010@users.noreply.github.com>
2024-11-04 12:04:31 +0600

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,62 @@
+package depthfirstpaths;
+
+
+public class Edge implements Comparable<Edge> {
+
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public Edge(int v, int w, double weight) {
+        if (v < 0) throw new IllegalArgumentException("vertex index must be a non-negative integer");
+        if (w < 0) throw new IllegalArgumentException("vertex index must be a non-negative integer");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public int either() {
+        return v;
+    }
+
+    public int other(int vertex) {
+        if      (vertex == v) return w;
+        else if (vertex == w) return v;
+        else throw new IllegalArgumentException("Illegal endpoint");
+    }
+
+    /**
+     * Compares two edges by weight.
+     * Note that {@code compareTo()} is not consistent with {@code equals()},
+     * which uses the reference equality implementation inherited from {@code Object}.
+     *
+     * @param  that the other edge
+     * @return a negative integer, zero, or positive integer depending on whether
+     *         the weight of this is less than, equal to, or greater than the
+     *         argument edge
+     */
+    @Override
+    public int compareTo(Edge that) {
+        return Double.compare(this.weight, that.weight);
+    }
+
+    /**
+     * Returns a string representation of this edge.
+     *
+     * @return a string representation of this edge
+     */
+    public String toString() {
+        return String.format("%d-%d %.5f", v, w, weight);
+    }
+
+
+    public static void main(String[] args) {
+        Edge e = new Edge(12, 34, 5.67);
+        System.out.println(e);
+    }
+}
